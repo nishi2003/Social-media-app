faf42e07a4fba4924a7b8ccff94447f8da8f3a6e
nishi2003 <nishinp2003@gmail.com>
2024-12-31 17:04:52 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,52 @@
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+
+// async function connectToMongoDB() {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('Connected to MongoDB Atlas!');
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB:', error.message);
+//   }
+// }
+
+// connectToMongoDB();
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import AuthRoute from './Routes/AuthRoute.js'; // Ensure .js extension is added
+import UserRoute from './Routes/UserRoute.js'
+import PostRoute from './Routes/PostRoute.js'
+import { registerUser } from './Controllers/AuthController.js';
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
+dotenv.config();
+
+// Database connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`Listening at ${process.env.PORT}`)
+    )
+  )
+  .catch((error) => console.log(error));
+
+// Usage of routes
+app.use('/auth', AuthRoute);
+// app.use('/register', registerUser)
+app.use('/user',UserRoute)
+app.use('/post',PostRoute)
